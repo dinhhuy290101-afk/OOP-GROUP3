a30f698e9a6262fdc8fe73b93a83a3e9c2c5c138
huyn2900 <dinhhuy290101@gmail.com>
2026-04-13 15:51:34 +0700

    Update InputHandler.h

--- src/InputHandler.h
+++ src/InputHandler.h
@@ -1,17 +1,10 @@
 #pragma once
 #include <string>
 #include "GameComponent.h"
-
 using namespace std;
 
-// Class xử lý input
 class InputHandler : public GameComponent {
 public:
-    // TODO: nhập từ người chơi
     string getInput();
-
-    // TODO: so sánh với từ gốc
-    bool checkAnswer(string input, string correct);
-
     void info() override;
 };
